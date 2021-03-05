f7db5d8ebb075b148f7f302fdaa0649d00b16e1c
Joshua Byrd <phocks@gmail.com>
2021-03-05 15:45:23 +1000

    OK MVP!!!

--- _scripts/index.js
+++ _scripts/index.js
@@ -3,4 +3,4 @@ const smartquotes = require("smartquotes");
 // Turns all quotation marks in page into smark quotes
 smartquotes();
 
-console.log("Hello from rollup... :)");
+console.log("Hello from rollup... :) please add some npm scripts here...");
