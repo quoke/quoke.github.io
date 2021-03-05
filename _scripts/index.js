581f7406cac4c5307e997f2eccc6f3be4e0938c2
Joshua Byrd <phocks@gmail.com>
2021-03-05 12:31:11 +1000

    Remove snowpack and add rollup

---
+++ _scripts/index.js
@@ -0,0 +1,6 @@
+const smartquotes = require("smartquotes");
+
+// Turns all quotation marks in page into smark quotes
+smartquotes();
+
+console.log("Hello from rollup... :)");
---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import { terser } from "rollup-plugin-terser";
+import path from "path";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import glob from "glob";
+
+const scriptPattern = path.resolve(__dirname, "_scripts/**/!(_)*.js");
+
+const inputs = glob.sync(scriptPattern).reduce((files, input) => {
+  const parts = input.split("/");
+  const fileKey = parts[parts.length - 1];
+  return { [fileKey]: input, ...files };
+}, {});
+
+const outputs = Object.keys(inputs).reduce((files, file) => {
+  const inputPath = inputs[file];
+  const parts = inputPath.split("/");
+  const pathIndex = parts.indexOf("_scripts") + 1;
+  const outputPath = parts.slice(pathIndex).join("/");
+  return { [file]: `assets/js/${outputPath}`, ...files };
+}, {});
+
+const bundles = Object.keys(inputs).map((key) => {
+  return {
+    input: inputs[key],
+    output: {
+      file: outputs[key],
+      format: "umd",
+      sourcemap: true,
+      exports: "default",
+      name: "quoke"
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      babel({
+        exclude: "node_modules/**",
+        comments: false,
+      }),
+      terser(),
+    ],
+  };
+});
+
+export default bundles;
